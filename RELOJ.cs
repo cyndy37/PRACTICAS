91fed241035506155696dee28be527894a56e220
cyndy37 <cyndyvolcom3722@hotmail.com>
2015-03-10 13:46:24 -0600

    RELOJ

---
+++ RELOJ.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            while (true)
+            {
+               
+               Console.Clear();
+               Console.WriteLine("\n     ********  RELOJ  ********");
+               Console.WriteLine("\n******** LA HORA ACTUAL ES:******** \n");
+
+
+               Console.WriteLine ("\n"+DateTime.Now.ToLongTimeString() );
+               Thread.Sleep(1000);
+          }
+     }
+} 
+
+
+
+        }
+    
+
